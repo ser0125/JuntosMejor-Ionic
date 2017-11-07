5914d35235fcd194c073d438838689ec2bf7301c
dcamilobj <dcamilobj@gmail.com>
2017-11-07 07:48:40 -0500

    Added take a photo method without save it

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { MainPage } from '../main/main';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 /**
  * Generated class for the RegisterPage page.
  *
@@ -15,8 +16,26 @@ import { MainPage } from '../main/main';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public camera: Camera) {
   }
+
+  takePhoto(){
+    const options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+   // imageData is either a base64 encoded string or a file URI
+   // If it's base64:
+   let base64Image = 'data:image/jpeg;base64,' + imageData;
+  }, (err) => {
+    console.log("Error")
+  });
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
